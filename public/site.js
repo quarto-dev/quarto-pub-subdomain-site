3ecb8eddfc573cbccfbcabf2dc78dac57df984f3
Brian Lambert <brianlambert@gmail.com>
2022-06-18 13:54:40 -0700

    Updates

--- public/site.js
+++ public/site.js
@@ -25,19 +25,36 @@ $(document).ready(async () => {
   }
 
   // Fetch the user sites for the subdomain.
+  //`http://localhost:3000/api/v1/subdomains/${subdomain}/sites`
   const response = await fetch(
-    `https://quartopub.org/api/v1/subdomains/${subdomain}/sites`,
-    {
-      method: "GET",
-    }
+    `https://quartopub.org/api/v1/subdomains/${subdomain}/sites`
   );
 
   const sites = await response.json();
 
+  console.log(sites);
+
   $("#description").text(`This is ${subdomain}'s site.`);
 
   for (const site of sites) {
-    $("#sites").append(`<div><a href="${site}">${site}</a></div>`);
+    const updatedDate = new Date(site.updated_timestamp);
+    $("#sites").append(`
+    <div>
+      <div>
+        ${site.title} (${site.type}) created by ${site.created_by.given_name} ${
+      site.created_by.surname
+    } (${site.created_by.username})
+      </div>
+      <div>
+        Last updated: ${updatedDate.toLocaleDateString()} at ${updatedDate.toLocaleTimeString()}
+      </div>
+      <div class="linkie">
+        <a href="${site.site_url}">${site.title}</a>
+      </div>
+      <img class="foo" src="${site.screenshot_url}">
+    </div>
+    `);
+
     console.log(`We have a user site: ${site}`);
   }
 });
