77812a19204b4d0346c41bc01ffb95ef674d277c
Brian Lambert <brianlambert@gmail.com>
2022-06-19 15:38:34 -0700

    Updates

--- public/site.js
+++ public/site.js
@@ -4,18 +4,26 @@ $(document).ready(async () => {
   const sites = await getSites();
   console.log(sites);
 
-  const source = $("#rocks-template").html();
-  var template = Handlebars.compile(source);
-
-  $(".name").text(subdomain);
+  var siteTemplate = Handlebars.compile($("#site-template").html());
+  $("#name").text(subdomain);
 
   $("#description").append(`<div>This is ${subdomain}'s site.</div>`);
-  $("#description").append(template({ doesWhat: "rocks!" }));
+  // $("#description").append(template({ doesWhat: "rocks!" }));
 
   for (const site of sites) {
     const updatedDate = new Date(site.updated_timestamp);
-    $("#sites").append(`
-    <div class="mt-10">
+    $("#sites").append(siteTemplate(site));
+  }
+
+  $("body").show();
+});
+
+/*
+
+
+      `
+    <div class="mt-3">
+      <img class="foo" src="${site.screenshot_url}">
       <div>
         ${site.title} (${site.type}) created by ${site.created_by.given_name} ${
       site.created_by.surname
@@ -27,10 +35,6 @@ $(document).ready(async () => {
       <div class="linkie">
         <a href="${site.site_url}">${site.title}</a>
       </div>
-      <img class="foo" src="${site.screenshot_url}">
     </div>
     `);
-  }
-
-  $("body").show();
-});
+    */
