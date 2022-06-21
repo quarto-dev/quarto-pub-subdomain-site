4cda9f7d9d77af566951cec00e4ca3af1df807e5
Brian Lambert <brianlambert@gmail.com>
2022-06-20 22:33:41 -0700

    Make list / grid accessible

--- public/app.js
+++ public/app.js
@@ -9,6 +9,9 @@ $(document).ready(async () => {
   var sitesTemplate = Handlebars.compile($("#sites-template").html());
   var siteTemplate = Handlebars.compile($("#site-template").html());
 
+  // State.
+  let gridView = true;
+
   // Load the sites.
   const sites = await getSites();
   console.log(sites);
@@ -16,19 +19,40 @@ $(document).ready(async () => {
   // Construct the site.
   $("#application").append(headerTemplate({ subdomain }));
   $("#application").append(displayOptionsTemplate());
-  $("#list-grid-control").click(() => {
-    $(".list-grid-control-indicator").toggleClass(
-      "list-grid-control-indicator-right"
-    );
-  });
-
   $("#application").append(sitesTemplate());
-
   for (const site of sites) {
     console.log(site);
     const updatedDate = new Date(site.updated_timestamp);
     $("#sites").append(siteTemplate(site));
   }
+
+  // Toggle list / grid control event handler.
+  const toggleListGridControlEventHandler = (event) => {
+    // Reject keyboard events that are not supported.
+    if (event.originalEvent instanceof KeyboardEvent) {
+      if (!(event.code == "Space" || event.code == "Enter")) {
+        return;
+      }
+    }
+
+    // Toggle the list / grid control.
+    if ((gridView = !gridView)) {
+      $(".list-grid-control-indicator").addClass(
+        "list-grid-control-indicator-right"
+      );
+      $("#list-grid-control").attr("aria-pressed", true);
+    } else {
+      $(".list-grid-control-indicator").removeClass(
+        "list-grid-control-indicator-right"
+      );
+      $("#list-grid-control").attr("aria-pressed", false);
+    }
+  };
+
+  // Add event handlers.
+  $("#list-grid-control")
+    .click(toggleListGridControlEventHandler)
+    .keydown(toggleListGridControlEventHandler);
 
   // Hide the spinner.
   $("#loading-spinner").hide();
